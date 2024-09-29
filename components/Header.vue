52365cfd4d587a8c6bd1959a0a6162e29f7ad9f4
SenaNur3 <se_na_e12@hotmail.com>
2024-09-29 23:30:56 +0300

    firstü

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <a-radio-group v-model:value="value" name="radioGroup">
+                <a-radio :value="1">List View</a-radio>
+                <a-radio :value="2">Grid View</a-radio>
+            </a-radio-group>
+        </div>
+        <div></div>
+    </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+const value = ref('1');
+</script>
